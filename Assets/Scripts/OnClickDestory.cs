dbf419df2b1a2fd1f700e3bb3a9f887fdd130826
BlakeTaylor332 <Blakeallentaylor1@gmail.com>
2015-12-28 19:20:58 -0600

    Play Default Level with changes
    Added 1 image to lower left block.  Added line of code to destroy 1
    image.

--- Assets/Scripts/OnClickDestory.cs
+++ Assets/Scripts/OnClickDestory.cs
@@ -1,34 +1,43 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OnClickDestory : MonoBehaviour {
-	public GameObject gameObj;
-	Animator ani;
-	bool _isAlive;
+public class OnClickDestory : MonoBehaviour
+{
+    public GameObject gameObj;
+    public GameObject squareOne;
+    Animator ani;
+    bool _isAlive;
 
-	// Use this for initialization
-	void Start () {
-		ani = GetComponent<Animator>();
-		gameObj = this.gameObject;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetMouseButtonDown(0)){
-			RaycastHit hit;
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			//Transform select = gameObj.transform;
-			if (Physics.Raycast(ray,out hit,100)){
-				//select.tag = "Untagged";
-				//hit.collider.transform.tag = "Player";
-				//Debug.Log("???");
-				_isAlive = hit.collider == gameObj.GetComponent<BoxCollider>();
-				ani.SetBool("isClicked", _isAlive);
-			}
-		}
-	}
-	void DestoryObject(){
-		Destroy(gameObj);
-	}
+
+    // Use this for initialization
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        gameObj = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //Transform select = gameObj.transform;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                //select.tag = "Untagged";
+                //hit.collider.transform.tag = "Player";
+                //Debug.Log("???");
+                _isAlive = hit.collider == gameObj.GetComponent<BoxCollider>();
+                ani.SetBool("isClicked", _isAlive);
+            }
+        }
+    }
+    void DestoryObject()
+    {
+        Destroy(squareOne);
+        Destroy(gameObj);
+    }
 }
 
