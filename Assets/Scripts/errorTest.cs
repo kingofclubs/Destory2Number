ee7078370ac866ea0c6ac797ec737afcbba7ef99
BlakeTaylor332 <Blakeallentaylor1@gmail.com>
2015-12-28 19:56:55 -0600

    The Beginnings: Error Test
    Created empty object with beginnings of error test script created and
    attached. Created public GameObject for each cube and assigned.

---
+++ Assets/Scripts/errorTest.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class errorTest : MonoBehaviour {
+    public GameObject cubeOne;
+    public GameObject cubeTwo;
+    public GameObject cubeThree;
+    public GameObject cubeFour;
+    public GameObject cubeFive;
+    public GameObject cubeSix;
+    public GameObject cubeSeven;
+    public GameObject cubeEight;
+    public GameObject cubeNine;
+
+    public class errorCheck
+    { 
+     
+
+    }
+}
+
